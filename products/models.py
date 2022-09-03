6475da624904908bb9fd58bf45f5e958a8f423a4
joachimnorden <joachim.norden@gmail.com>
2022-09-03 17:56:32 +0000

    add sizes to product model and size selector box to product template

--- products/models.py
+++ products/models.py
@@ -28,7 +28,7 @@ class Product(models.Model):
     sale_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     discount = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     is_new = models.BooleanField(default=True)
-    color = models.CharField(max_length=254, null=True, blank=True)
+    has_colors = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     review_count = models.DecimalField(max_digits=6, decimal_places=0, null=True, blank=True, default=0)
