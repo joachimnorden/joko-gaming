ffb427fb8d335df51515a4729ace21e575ee07e5
joachimnorden <joachim.norden@gmail.com>
2022-09-03 14:42:17 +0000

    Add products view, urls and html template

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    """ product model display """
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'review_count',
+        'image'
+    )
+
+    search_fields = ('name', 'brand', 'price',)
+    ordering = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """category model dissplay"""
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+    search_fields = ('friendly_name', 'name',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+
