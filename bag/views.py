ceac3a140c54c2cea45b7a77c0e98050d8625bc2
joachimnorden <joachim.norden@gmail.com>
2022-09-04 11:28:03 +0000

    Add adjust bag and remove from bag view, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
-# Create your views here.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -33,4 +35,73 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    color = None
+    if 'product_color' in request.POST:
+        size = request.POST['product_color']
+    bag = request.session.get('bag', {})
+
+    if color:
+        if quantity > 0:
+            bag[item_id]['items_by_color'][color] = quantity
+            messages.success(request,
+                             (f'Updated color {color.upper()} '
+                              f'{product.name} quantity to '
+                              f'{bag[item_id]["items_by_color"][color]}'))
+        else:
+            del bag[item_id]['items_by_color'][color]
+            if not bag[item_id]['items_by_color']:
+                bag.pop(item_id)
+            messages.success(request,
+                             (f'Removed color {color.upper()} '
+                              f'{product.name} from your bag'))
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+            messages.success(request,
+                             (f'Updated {product.name} '
+                              f'quantity to {bag[item_id]}'))
+        else:
+            bag.pop(item_id)
+            messages.success(request,
+                             (f'Removed {product.name} '
+                              f'from your bag'))
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        color = None
+        if 'product_color' in request.POST:
+            color = request.POST['product_color']
+        bag = request.session.get('bag', {})
+
+        if color:
+            del bag[item_id]['items_by_color'][color]
+            if not bag[item_id]['items_by_color']:
+                bag.pop(item_id)
+            messages.success(request,
+                             (f'Removed color {color.upper()} '
+                              f'{product.name} from your bag'))
+        else:
+            bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
     
