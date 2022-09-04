1cb13c61af5fc0eea71adfe459b846f2fbce958e
joachimnorden <joachim.norden@gmail.com>
2022-09-04 10:22:15 +0000

    Finished product color logic and add product image style

--- bag/views.py
+++ bag/views.py
@@ -4,21 +4,33 @@ from django.shortcuts import render, redirect
 
 def view_bag(request):
     """ A view that renders the bag contents page """
-    return render(request, 'bag/bag.html')
 
+    return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    color = None
+    if 'product_color' in request.POST:
+        color = request.POST['product_color']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if color:
+        if item_id in list(bag.keys()):
+            if color in bag[item_id]['items_by_color'].keys():
+                bag[item_id]['items_by_color'][color] += quantity
+            else:
+                bag[item_id]['items_by_color'][color] = quantity
+        else:
+            bag[item_id] = {'items_by_color': {color: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
+    
