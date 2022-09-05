d3bc2f169ded0cb166a75f33a39f8ac2beda0a62
joachimnorden <joachim.norden@gmail.com>
2022-09-05 14:31:49 +0000

    Add adming product view, url and template

--- products/views.py
+++ products/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
+from .forms import ProductForm
+
 
 def all_products(request):
     """ A view to show all products, including sorting and searching queries"""
@@ -69,3 +72,15 @@ def product_detail(request, product_id):
         'product': product,
     }
     return render(request, 'product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
