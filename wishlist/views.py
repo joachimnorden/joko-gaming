1d8f551c4342b7275dd5899ba166e172ac284ddb
joachimnorden <joachim.norden@gmail.com>
2022-09-05 19:02:18 +0000

    Add wishlist view and url

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from products.models import Product
+from profiles.models import UserProfile
+from .models import Wishlist
 
-# Create your views here.
+
+@login_required
+def wishlist(request):
+    """ wishlist page """
+
+    template = 'wishlist/wishlist.html'
+    context = {
+        'on_page': True,
+    }
+    return render(request, template, context)
