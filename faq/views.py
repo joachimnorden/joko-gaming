27c319aebf4de940eb6219d333e76e7223169c6e
joachimnorden <joachim.norden@gmail.com>
2022-09-06 04:56:37 +0000

    Add faq view, url and template

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Question
 
-# Create your views here.
+
+def faq_page(request):
+    """ FAQ's page view"""
+
+    questions = Question.objects.all().filter(status=1).order_by('-created_on')
+    context = {
+        'questions': questions
+    }
+    template = 'faq/faq.html'
+
+    return render(request, template, context)
