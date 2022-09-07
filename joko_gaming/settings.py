90140e79a92213c66dab7d94eaab1b5bebe33d55
joachimnorden <joachim.norden@gmail.com>
2022-09-07 18:40:20 +0000

    Removed secret key and set debug

--- joko_gaming/settings.py
+++ joko_gaming/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^8()zce&88uw_)iz6^a7wdxrixeo*miivk!rxh6pv-wnwddg9e' # noqa
+SECRET_KEY = os.environ.get('SECRET_KEY', '') # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['joko-gaming.herokuapp.com', 'localhost']
 
